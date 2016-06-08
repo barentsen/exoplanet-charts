80014c0e6ac234ade616624e431c8e29c967b5d0
Geert Barentsen <geert@barentsen.be>
2016-06-07 23:00:04 -0700

    Improves the atmospheric characterization plot

--- k2-planets/k2-planets-for-atmospheric-characterization.py
+++ k2-planets/k2-planets-for-atmospheric-characterization.py
@@ -5,10 +5,17 @@ from matplotlib import style
 import seaborn as sns
 import pandas as pd
 
+SHOW_KEPLER = False
+K_MAGNITUDE_CUT = 11
+OUTPUT_PREFIX = 'k2-planets-for-atmospheric-characterization'
+OUTPUT_SUFFIX = '.png'
+
+if SHOW_KEPLER:
+    OUTPUT_PREFIX += '-with-kepler'
+
 palette = sns.color_palette(['#f1c40f', '#2980b9'])
 style.use('../styles/black.mplstyle')
 
-K_MAGNITUDE_CUT = 11
 k2_df = pd.read_csv('../data/k2-planets.csv')
 k2_df = k2_df[k2_df.st_k2 <= K_MAGNITUDE_CUT]
 
@@ -17,31 +24,58 @@ print('Plotting {} points.'.format(len(k2_df)))
 fig = pl.figure()
 pl.fill_between([.5, .5, 2.5, 2.5, .5],
                 [2000, 4000, 4000, 2000, 2000],
-                lw=0, zorder=-1, alpha=0.3,
-                facecolor=palette[1], edgecolor=palette[1])
-pl.scatter(k2_df.pl_rade, k2_df.st_teff, lw=0.4, s=35, label='K2',
-           facecolor=palette[0], edgecolor='black')
+                zorder=-1, alpha=0.3,
+                facecolor=palette[1],
+                lw=0)
+pl.plot([.5, .5, 2.5, 2.5, .5],
+                [2000, 4000, 4000, 2000, 2000],
+                zorder=-1, alpha=1,
+                color='white',
+                lw=1.5, linestyle='dotted',
+                dashes=[2, 4])
+pl.scatter(k2_df.pl_rade, k2_df.st_teff,
+           lw=0.4, s=35, label='K2',
+           facecolor=palette[0], edgecolor='black',
+           zorder=30)
+
+if SHOW_KEPLER:
+    kepler_df = pd.read_csv('../data/kepler-candidates.csv')
+    kepler_df = kepler_df[kepler_df.koi_kmag <= K_MAGNITUDE_CUT]
+    pl.scatter(kepler_df.koi_prad, kepler_df.koi_steff,
+               lw=0.4, s=35, label='Kepler',
+               facecolor='#2980b9', edgecolor='black',
+               zorder=20)
+
+pl.legend(bbox_to_anchor=(0., 1., 1., 0.),
+          loc=8,
+          ncol=2,
+          borderaxespad=0.,
+          handlelength=0.8,
+          frameon=False)
 
 # Annotations
 pl.annotate("Earth and Super Earth-size Candidates\n"
             "Orbiting Cool Dwarfs",
+            style='italic',
             xy=(2.5, 2900), xycoords='data',
             xytext=(2.8, 2900), textcoords='data',
             va="center", size=12,
-            arrowprops=dict(arrowstyle="-")
+            arrowprops=dict(arrowstyle="-", lw=1)
             )
-pl.suptitle("K2 Planet Candidates for Atmospheric Characterization (Ks < 11)")
+pl.suptitle("Planet Candidates for Atmospheric Characterization (Ks < 11)")
 
 # Axes
 pl.yticks([3000, 4000, 5000, 6000, 7000],
           ['3,000 K', '4,000 K', '5,000 K', '6,000 K', '7,000 K'])
-pl.axes().xaxis.set_minor_locator(MultipleLocator(0.2))
-pl.axes().yaxis.set_minor_locator(MultipleLocator(200))
+pl.axes().xaxis.set_minor_locator(MultipleLocator(0.5))
+pl.axes().yaxis.set_minor_locator(MultipleLocator(500))
 pl.xlim(0.0, 5)
 pl.ylim(2250, 7000)
 pl.xlabel('Planet Size Relative to Earth (Radius)')
 pl.ylabel('Host Star Temperature')
 
 pl.tight_layout(rect=(0, 0, 1, 0.92))
-pl.savefig("k2-planets-for-atmospheric-characterization.png", dpi=200)
+output_fn = OUTPUT_PREFIX + OUTPUT_SUFFIX
+print('Writing {}'.format(output_fn))
+pl.savefig(output_fn, dpi=200)
 pl.close()
